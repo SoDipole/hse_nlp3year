81fd99538f5cfbfc946b5214b720d2af11f894c4
Polina Sonina <sarun4ik7@yandex.ru>
2017-03-17 11:04:47 +0300

    tokenize abbreviations

--- project_preprocessing/tokenizer.py
+++ project_preprocessing/tokenizer.py
@@ -5,8 +5,8 @@ import csv
 tokens = ''
 
 abbrev_file = open('abbreviations.txt', 'r', encoding = 'utf-8')
-s1 = abbrev_file.read()
-abbreviations = s1.split() # keep with .
+abbreviations = abbrev_file.read().split() # keep with .
+abbreviations_masked = [ab.replace('.', '###') for ab in abbreviations]
 
 punctuation = ['\.', '\,\s', '\"', '\?\!', '\?\?\?', '\!\!\!', '[^\?](\?)[^\?!]', '[^\!\?](\!)[^\!]',
                '[^\d](\:)[^\d]', '\;', '\(', '\)', '…', '—', '©', '[^\d](\/)[^\d]']
@@ -16,20 +16,26 @@ for root, dirs, files in os.walk('./reviews_clean'):
     for file in files:
         if file.endswith('.txt'):
             f = open(root + '/' + file, 'r', encoding = 'utf-8')
-            s2 = f.read()
+            text = f.read()
+            
+            for i in range(len(abbreviations)):
+                text = text.replace(abbreviations[i], abbreviations_masked[i])
             
             for i in range(len(punctuation)):
                 search = punctuation[i]
-                punct_in_s2 = re.findall(search, s2)
+                punct_in_text = re.findall(search, text)
                 
-                if punct_in_s2:
-                    for m in punct_in_s2:
-                        s2 = s2.replace(m, ' ' + m + ' ')
+                if punct_in_text:
+                    for m in punct_in_text:
+                        text = text.replace(m, ' ' + m + ' ')
             
-            text = s2.split() # list of words
+            for i in range(len(abbreviations)):
+                text = text.replace(abbreviations_masked[i], abbreviations[i])            
+            
+            words = text.split() # list of words
                              # separates names like 'James M. Cain'
             token_id = 1
-            for x in text:
+            for x in words:
                 fields = str(doc_id) + '\t' + str(token_id) + '\t' + x + '\n'
                 tokens += fields
                 
